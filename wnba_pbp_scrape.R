18bff17ee62c49478f6972fd97157b4c34d8c7a0
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2021-06-07 14:50:07 -0600

    Cleaned up and commented the parsing function script

--- wnba_pbp_scrape.R
+++ wnba_pbp_scrape.R
@@ -121,12 +121,21 @@ possession_data <- function(gameid, data){
         
         # Create an empty vector for the players going out 
         players_out_away <- character(length(Awaysubs))
+        
+        # If there are any away substitutions determine who they are
         if(!identical(Awaysubs, character(0))){
+          # Extract players coming in and coming out
           players_out_away <- str_trim(str_extract(Awaysubs, "(?<=for )(.*)$"))
           players_in_away <- str_trim(str_extract(Awaysubs, "^(.*)(?=enters)"))
+          
+          # Pull out the first and last name for the player 
+          # (Basketball Reference lists players as first initial last name i.e. D. Teuscher)
           for(s in 1:length(Awaysubs)){
+            # From the box score data, the first and last name is extracted 
             players_in_away[s] <- box_score$athlete_display_name[str_detect(box_score$athlete_short_name, players_in_away[s])]
             players_out_away[s] <- box_score$athlete_display_name[str_detect(box_score$athlete_short_name, players_out_away[s])]
+            
+            # Fixing a few of the names that throw errors
             if(str_detect(players_out_away[s], "Brittany Boyd-Jones")){
               players_out_away[s] <- str_replace(players_out_away[s], "Brittany Boyd-Jones", "Brittany Boyd")
             }
@@ -146,6 +155,7 @@ possession_data <- function(gameid, data){
               players_in_away[s] <- str_replace(players_in_away[s], "Natisha Hiedeman", "Natasha Hiedeman")
             }
             
+            # Replacing players in the away lineup (Might need to adust some of this)
             if(str_detect(LineupAway[i], players_out_away[s])){
               LineupAway[i] <- str_replace(LineupAway[i], players_out_away[s], players_in_away[s])
               LineupHome[i] <- LineupHome[i]
@@ -155,13 +165,22 @@ possession_data <- function(gameid, data){
             }
           }
         }
+        
+        # Create empty vector for home substitutions
         players_out_home <- character(length(Homesubs))
+        
+        # If there are substitutions for the home team, figure out who they are
         if(!identical(Homesubs, character(0))){
+          # Extract the players coming in and out
           players_out_home <- str_trim(str_extract(Homesubs, "(?<= for)(.*)$"))
           players_in_home <- str_trim(str_extract(Homesubs, "^(.*)(?=enters)"))
+          
+          # Extract the first and last name for the play
          for(s in 1:length(Homesubs)){
            players_in_home[s] <- box_score$athlete_display_name[str_detect(box_score$athlete_short_name, players_in_home[s])]
            players_out_home[s] <- box_score$athlete_display_name[str_detect(box_score$athlete_short_name, players_out_home[s])]
+           
+           # Fix names that cause errors
            if(str_detect(players_out_home[s], "Brittany Boyd-Jones")){
              players_out_home[s] <- str_replace(players_out_home[s], "Brittany Boyd-Jones", "Brittany Boyd")
            }
@@ -181,6 +200,7 @@ possession_data <- function(gameid, data){
              players_in_home[s] <- str_replace(players_in_home[s], "Natisha Hiedeman", "Natasha Hiedeman")
            }
            
+           # Replace the player in the home lineup (This section needs to be changed also)
            if(str_detect(LineupAway[i], players_out_home[s])){
              LineupAway[i] <- str_replace(LineupAway[i], players_out_home[s], players_in_home[s])
              LineupHome[i] <- LineupHome[i]
@@ -190,20 +210,34 @@ possession_data <- function(gameid, data){
            }
          }
         }
+        # All of the players being subbed out
         players_out <- c(players_out_away, players_out_home) 
+        
+        # Substitution if there is only one substitution being done
       } else {
       # Determine the player coming in and the player coming out of the game
+        # If the player coming out is in the either the home or away lineup, then make the 
+        # substitution
         if(str_detect(LineupHome[i-1], player_out) | str_detect(LineupAway[i-1], player_out)){
+          # If one of them is missing extract information from Basketball Reference
           if(player_in == "" || player_out == ""){
+            # Filter for the specific game
             game <- game_info %>% filter(game_id == gameid)
+            # Create a string for the url
             url <- paste0("https://www.basketball-reference.com/wnba/boxscores/pbp/", game$game_day, "0",game$bref_home, ".html")
+            # Extract the play by play data using rvest
             table <- url %>% read_html() %>% html_table()
             table[[1]] <- table[[1]][-1,]
             table <- url %>% read_html() %>% html_table()
             colnames(table[[1]]) <- table[[1]][1,]
             bref_pbp <- table[[1]]
+            # Rename the play by play columns
             colnames(bref_pbp) <- c("Time", "Away", "Away_Points", "Score", "Home_Points", "Home")
+            # Filter for all plays at the time of the occurance
             possible_vals <- bref_pbp %>% filter(Time == paste0(pbp$clock_display_value[i], ".0"))
+            # If the player coming out is unknown, pull the first initial and last name 
+            # for the player coming in; else pull the first initial and last name for the player
+            # coming out
             if(player_out == ""){
               first_inital <- str_extract(player_in, "^[A-Z]")
               last_name <- str_extract(player_in, "[A-Za-z]+$")
@@ -213,41 +247,55 @@ possession_data <- function(gameid, data){
               last_name <- str_extract(player_out, "[A-Za-z]+$") 
               player_name <- paste0(first_inital, ". ", last_name)
             }
+            # If there is only one entry at the time of the substitution that contains the players
+            # name, pull that row from the play by play. If there are multiple plays containing a 
+            # player at the time, only pull the rows that includes the substitution
             if(sum(str_detect(possible_vals$Away, player_name)) == 1){
               sub <- possible_vals$Away[str_detect(possible_vals$Away, player_name)]
               if(length(sub) > 1){
                 sub_play <- str_detect(sub, "enters the game")
                 sub <- sub[sub_play]
               }
+              # Extract either the player coming in or coming out depending on which is missing
               if(player_out == ""){
                 player <- str_extract(sub, "(?<=for)(.*)$")
               } else {
                 player <- str_extract(sub, "^(.*)(?= enters)")
-              }
+              } 
+              # Set to an empty string if the substitution doesn't occur correctly 
               if(!identical(player, character(0)) && is.na(player)){
                 player <- ""
               }
+              # If the player isn't on the away team, do the same process for the home team
             } else {
+              # Find the rows where the player is included and find the row where a substitution 
+              # occurs
               sub <- possible_vals$Home[str_detect(possible_vals$Home, player_name)]
               if(length(sub) > 1){
                 sub_play <- str_detect(sub, "enters the game")
                 sub <- sub[sub_play]
               }
+              # Extract either the player coming in or coming out
               if(player_out == ""){
                 player <- str_extract(sub, "(?<=for)(.*)$")
               } else {
                 player <- str_extract(sub, "^(.*)(?= enters)")
               }
+              
+              # This may need to be adjusted as well 
               player <- str_extract(sub, "[A-Z.]+ [A-Za-z]+$")
               if(!identical(player, character(0)) && is.na(player)){
                 player <- ""
               }
             }
+            # Set the player to be empty if there is no sub extracted; Else extract the first and 
+            # last name from the box score data
             if(identical(player, character(0))){
               player <- ""
               player_sub <- player
             } else {
               player_sub <- box_score$athlete_display_name[str_detect(box_score$athlete_short_name, player)]
+              # Control for names that throw errors
               if(str_detect(player_sub, "Brittany Boyd-Jones")){
                 player_sub <- str_replace(player_sub, "Brittany Boyd-Jones", "Brittany Boyd")
               }
@@ -259,6 +307,8 @@ possession_data <- function(gameid, data){
               }
               
             }
+            # Set player in or player out as the player that was extracted from 
+            # Basketball reference
             if(player_in == ""){
               player_in <- player_sub
               players_out <- player_out
@@ -270,6 +320,7 @@ possession_data <- function(gameid, data){
         }
       }
       
+      # If any of the players coming out are in the lineup, sub them out
       if(any(str_detect(LineupAway[i-1], players_out)) | any(str_detect(LineupHome[i-1], players_out))){
       # If the player going out is on the away team, substitute the player in on the 
       # away team. If they aren't on the away team, then sub them for the home team
@@ -282,6 +333,8 @@ possession_data <- function(gameid, data){
               LineupHome[i] <- str_replace(LineupHome[i-1], player_out, player_in)
               LineupAway[i] <- LineupAway[i-1]
             }
+            # If one of the players are still missing for some reason, keep the lineup the same 
+            # as previously
           } else{
             LineupAway[i] <- LineupAway[i-1]
             LineupHome[i] <- LineupHome[i-1]
@@ -299,28 +352,36 @@ possession_data <- function(gameid, data){
   
   # Combine the lineup with the play by play data
   test <- pbp %>% bind_cols(LineupAway = LineupAway, LineupHome = LineupHome)
-  #View(test %>% dplyr::select(text, LineupAway, LineupHome, clock_display_value))
+  
   # Select variables that are needed to pull out possession information
   possession <- test %>% dplyr::select(shooting_play, home_score, scoring_play, away_score,
                                 text, score_value, team_id, type_text, LineupAway,
                                 LineupHome, clock_display_value)
-  
+  # Vector with the different type of turnovers
   turnover_types <- c("Out of Bounds - Lost Ball Turnover", "Offensive Foul Turnover", "Shot Clock Turnover", "Bad Pass\nTurnover",
                       "Lost Ball Turnover", "Out of Bounds - Bad Pass Turnover", "Traveling")
   
+  # Create a vector for change of possession
   change_possession <- numeric(nrow(possession))
+  
+  # Conditions that cause change of possession
   cond <- (possession$shooting_play == TRUE & possession$scoring_play == TRUE) | 
     possession$type_text == "Defensive Rebound" | possession$type_text %in% turnover_types
   
+  # Free throws that don't change possession
   free_throws <- c("Free Throw - 1 of 2", "Free Throw - 1 of 3", "Free Throw - 2 of 3", "Free Throw - Technical")
   
   for(i in 2:nrow(possession)){
+    # If any of those conditions are met, possession changes
     if(cond[i] == TRUE){
       change_possession[i] <- 1
     }
+    # If a shooting foul occurs at the same time as a made basket, don't change possession
     if(possession$type_text[i] == "Shooting Foul" && (possession$clock_display_value[i] == possession$clock_display_value[i-1])){
       change_possession[i-1] <- 0
     }
+    # If the last free throw is made but a substitution was made before the last free throw
+    # Make the possession change before the free throw
     if((possession$type_text[i] == "Free Throw - 1 of 1" || 
         possession$type_text[i] == "Free Throw - 2 of 2" || 
         possession$type_text[i] == "Free Throw - 3 of 3") && 
@@ -328,22 +389,27 @@ possession_data <- function(gameid, data){
       change_possession[i] <- 0
       change_possession[i-1] <- 1
     }
+    # The possession doesn't change if the free throw isn't the last one
     if(possession$type_text[i] %in% free_throws){
       change_possession[i] <- 0
     }
     
   }
   
+  # Bind on the change possession variable
   possession <- possession %>% mutate(change_possession = change_possession)
 
-  
+  # Create vectors the length of the number of possesions for the point differential 
+  # and the lineups
   point_diff <- numeric(sum(possession$change_possession == 1) + 1)
   LineupAway <- numeric(length(point_diff))
   LineupHome <- numeric(length(point_diff))
   home_points <- numeric(length(point_diff))
   away_points <- numeric(length(point_diff))
+  # Pull the starting lineups again
   LineupAway[1] <- paste(starters$athlete_display_name[1:5], collapse = ",")
   LineupHome[1] <- paste(starters$athlete_display_name[6:10], collapse = ",")
+  # Fix edges cases (Can probably adjust this code as well )
   if(str_detect(LineupHome[1], "Brittany Boyd-Jones")){
     LineupHome[1] <- str_replace(LineupHome[1], "Brittany Boyd-Jones", "Brittany Boyd")
   } else if(str_detect(LineupAway[1], "Brittany Boyd-Jones")){
@@ -358,6 +424,8 @@ possession_data <- function(gameid, data){
   home_points[1] <- 0
   away_points[1] <- 0
   k <- 2
+  # Filter through each row in the play by play data and when the possession changes
+  # calculate the point differential for that possession
   for(i in 1:nrow(possession)){
     if(possession$change_possession[i] == 1){
       home_points[k] <-  possession$home_score[i]
@@ -368,133 +436,16 @@ possession_data <- function(gameid, data){
       k <- k + 1
     }
   }
+  # Create a data frame with the points for both teams at each possession as well the point
+  # differential for the possession and the lineups
   another <- data.frame(home_points, away_points, point_diff, LineupAway, LineupHome)
+  # Reshaping from wide to long format to make it easy to create X matrix for modeling 
   test2 <- another %>% 
     separate(LineupAway, into = c("P1", "P2", "P3", "P4", "P5"), sep = ",") %>%
     separate(LineupHome, into = c("P6", "P7", "P8", "P9", "P10"), sep = ",") %>% 
     pivot_longer(cols = P1:P10, values_to = "Player", names_to =  NULL) %>%
     mutate(Player = factor(Player))
+  # Return the data in long format
   return(test2)
 }
-
-# # Bring in the box score data and filter for only the starters to get the lineup at the 
-# # beginning of the game
-# box_score <- wehoop::espn_wnba_player_box(game_id = "401104913") %>%
-#   filter(starter == TRUE)
-# 
-# # Create empty vectors for the lineups for both teams
-# # Length is the same length as the play by play 
-# LineupAway <- character(nrow(pbp))
-# LineupHome <- character(nrow(pbp))
-# 
-# # Set the starting lineup for both teams; 
-# # Away team is always the first 5 players
-# # Home team is the second 5 players
-# LineupAway[1] <- paste(box_score$athlete_display_name[1:5], collapse = ",")
-# LineupHome[1] <- paste(box_score$athlete_display_name[6:10], collapse = ",")
-# 
-# # Loop through every row on the play by play data 
-# for(i in 2:nrow(pbp)){
-#   # If the play is a substitution, change the lineup
-#   if(pbp$type_text[i] == "Substitution"){
-#     # Determine the player coming in and the player coming out of the game
-#     player_in <- str_extract(pbp$text[i], "^^[A-Z][-a-zA-Z]+ [A-Z][-a-zA-Z]+")
-#     player_out <- str_extract(pbp$text[i], "[A-Z][-a-zA-Z]+ [A-Z][-a-zA-Z]+$")
-#     
-#     # If the player going out is on the away team, substitute the player in on the 
-#     # away team. If they aren't on the away team, then sub them for the home team
-#     if(str_detect(LineupAway[i -1], player_out)){
-#       LineupAway[i] <- str_replace(LineupAway[i-1], player_out, player_in)
-#       LineupHome[i] <- LineupHome[i-1]
-#     } else{
-#       LineupHome[i] <- str_replace(LineupHome[i-1], player_out, player_in)
-#       LineupAway[i] <- LineupAway[i-1]
-#     }
-#   }
-#   
-#   # If it isn't a substitution, keep the lineup the same as the previous play
-#   else{
-#     LineupAway[i] <- LineupAway[i-1]
-#     LineupHome[i] <- LineupHome[i-1]
-#   }
-# }
-# 
-# # Combine the lineup with the play by play data
-# test <- pbp %>% bind_cols(LineupAway = LineupAway, LineupHome = LineupHome)
-# 
-# # Select variables that are needed to pull out possession information
-# possession <- test %>% select(shooting_play, home_score, scoring_play, away_score,
-#                              text, score_value, team_id, type_text, LineupAway,
-#                              LineupHome, clock_display_value)
-# 
-# turnover_types <- c("Out of Bounds - Lost Ball Turnover", "Offensive Foul Turnover", "Shot Clock Turnover", "Bad Pass\nTurnover",
-#                     "Lost Ball Turnover", "Out of Bounds - Bad Pass Turnover", "Traveling")
-# 
-# change_possession <- numeric(nrow(possession))
-# cond <- (possession$shooting_play == TRUE & possession$scoring_play == TRUE) | 
-#   possession$type_text == "Defensive Rebound" | possession$type_text %in% turnover_types
-# 
-# free_throws <- c("Free Throw - 1 of 2", "Free Throw - 1 of 3", "Free Throw - 2 of 3", "Free Throw - Technical")
-# 
-# for(i in 2:nrow(possession)){
-#   if(cond[i] == TRUE){
-#     change_possession[i] <- 1
-#   }
-#   if(possession$type_text[i] == "Shooting Foul" && (possession$clock_display_value[i] == possession$clock_display_value[i-1])){
-#     change_possession[i-1] <- 0
-#   }
-#   if((possession$type_text[i] == "Free Throw - 1 of 1" || 
-#      possession$type_text[i] == "Free Throw - 2 of 2" || 
-#      possession$type_text[i] == "Free Throw - 3 of 3") && 
-#      (possession$type_text[i-1] == "Substitution")){
-#     change_possession[i] <- 0
-#     change_possession[i-1] <- 1
-#   }
-#   if(possession$type_text[i] %in% free_throws){
-#     change_possession[i] <- 0
-#   }
-#   
-# }
-# 
-# possession <- possession %>% mutate(change_possession = change_possession)
-# 
-# point_diff <- numeric(sum(possession$change_possession == 1) + 1)
-# LineupAway <- numeric(length(point_diff))
-# LineupHome <- numeric(length(point_diff))
-# home_points <- numeric(length(point_diff))
-# away_points <- numeric(length(point_diff))
-# LineupAway[1] <- paste(box_score$athlete_display_name[1:5], collapse = ",")
-# LineupHome[1] <- paste(box_score$athlete_display_name[6:10], collapse = ",")
-# home_points[1] <- 0
-# away_points[1] <- 0
-# k <- 2
-# for(i in 1:nrow(possession)){
-#   if(possession$change_possession[i] == 1){
-#     home_points[k] <-  possession$home_score[i]
-#     away_points[k] <- possession$away_score[i]
-#     LineupAway[k] <- possession$LineupAway[i]
-#     LineupHome[k] <- possession$LineupHome[i]
-#     point_diff[k] <- (home_points[k] - home_points[k-1]) - (away_points[k] - away_points[k-1])
-#     k <- k + 1
-#   }
-# }
-# another <- data.frame(home_points, away_points, point_diff, LineupAway, LineupHome)
-# test2 <- another %>% 
-#   separate(LineupAway, into = c("P1", "P2", "P3", "P4", "P5"), sep = ",") %>%
-#   separate(LineupHome, into = c("P6", "P7", "P8", "P9", "P10"), sep = ",") %>% 
-#   pivot_longer(cols = P1:P10, values_to = "Player", names_to =  NULL) %>%
-#   mutate(Player = factor(Player))
-# 
-# X <- model.matrix(point_diff ~ -1 + Player, data = test2)
-# ids <- seq(10, nrow(X), by = 10)
-# X_small <- matrix(0, nrow = nrow(X)/10, ncol = ncol(X))
-# colnames(X_small) <- colnames(X)
-# for(i in ids){
-#   k <- i/10
-#   for(j in 1:ncol(X)){
-#     X_small[k, j] <- sum(X[(i-9):i, j]) 
-#   }
-# }
-# 
-# X_small
 
